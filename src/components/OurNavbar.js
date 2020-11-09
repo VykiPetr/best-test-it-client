2e12e8df531712329e49a18814b41a1b460e535d
Vykintas.Petrikas <Vykintas.petrikas@gmail.com>
2020-11-09 15:46:59 +0200

    More fancy navbar, renders conitionally

--- src/components/OurNavbar.js
+++ src/components/OurNavbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 let textStyle = {
     textDecoration: 'none',
@@ -8,15 +8,29 @@ let textStyle = {
 
 function OurNavbar(props) {
 
+    const [LoggedInUser, setLoggedInUser] = useState({})
+    useEffect(() => {
+        setLoggedInUser(props.loggedIn)
+    })
 
-   
+
     return (
         <nav>
             <Link to='/' style={textStyle}>Landing </Link>
-            <Link to='/sign-up' style={textStyle}>Sign Up </Link>
-            <Link to='/sign-in' style={textStyle}>Sign In </Link>
+            {
+                LoggedInUser ? null : <Link to='/sign-up' style={textStyle}>Sign Up </Link>
+            }
+            {
+                LoggedInUser ? null : <Link to='/sign-in' style={textStyle}>Sign In </Link>
+            }
             <Link to='/view-projects' style={textStyle}>Projects </Link>
-            <Link to='/' style={textStyle} onClick={props.onLogout}>Logout </Link>
+            {
+                LoggedInUser ? <Link style={textStyle} to={`/profile/${LoggedInUser._id}`}>Profile </Link> : null
+            }
+            {
+                LoggedInUser ? <Link to='/' style={textStyle} onClick={props.onLogout}>Logout </Link> : null
+            }
+            
         </nav>
     )
 }
