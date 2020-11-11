a2aa5f7fdf663334f2d1043a65e2fc2db8544d81
ibirossi <ibi.homaidan@gmail.com>
2020-11-11 17:20:35 +0100

    preparing to pull

--- src/components/ProjectHeader.js
+++ src/components/ProjectHeader.js
@@ -3,34 +3,36 @@ import { Link } from "react-router-dom";
 import { Button, Card, Image } from "semantic-ui-react";
 
 function ProjectHeader(props) {
-  const {userRefId, appName, appLogo, likes, _id } = props.project;
+  const {
+    userRefId,
+    appName,
+    appLogo,
+    likes,
+    _id,
+    appDescription,
+  } = props.project;
 
   const handleProjectClick = () => {};
 
   return (
-    
-      <div>
-    
-      <Card.Group >
-        <Card style={{ backgroundColor: "#FFE3B3" }}>
-          <Card.Content >
+    <div >
+      <Card.Group>
+        <Card className="card-style" raised="true">
+          <Card.Content>
             <Image floated="left" size="mini" src={appLogo} />
 
-            <Card.Header>
-              <Link to={`/project/${_id}`}>
-                {appName}{" "}
-              </Link>
+            <Card.Header textAlign="left">
+              <Link to={`/project/${_id}`}>{appName} </Link>
             </Card.Header>
             <Link to={`/profile/${userRefId._id}`}>
-            <Card.Meta floated="right">{userRefId.username}</Card.Meta>
+              <Card.Meta floated="right">{userRefId.username}</Card.Meta>
             </Link>
-            <Card.Description>Some text here</Card.Description>
-            <Card.Meta>{likes.length} - Likes</Card.Meta>
+            <Card.Description>{appDescription}</Card.Description>
+            <Card.Meta textAlign="right">{likes.length} - Likes</Card.Meta>
           </Card.Content>
         </Card>
       </Card.Group>
-      </div>
-    
+    </div>
   );
 }
 
