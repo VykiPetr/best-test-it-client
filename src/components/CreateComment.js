c2fbb2a4356dc5eb57363663d612833a2a9adaf7
ibirossi <ibi.homaidan@gmail.com>
2020-11-12 09:56:45 +0100

    fixed user image in comment

--- src/components/CreateComment.js
+++ src/components/CreateComment.js
@@ -2,17 +2,20 @@ import React from "react";
 import { Button, Comment, Form, Image } from "semantic-ui-react";
 
 function CreateComment(props) {
+  console.log(props.data.userRefId)
   return (
+    
     <div>
       <Comment.Group >
         <Comment className="comment-container">
           <Image
             size="mini"
             as='a'
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQrwrCagQl7yLi6VRkar9jw9e2gEfFKTECi5g&usqp=CAU"
+            src={props.data.userRefId.userImage}
           />
           <Comment.Author as="a">{props.data.userRefId.username}</Comment.Author>
             <Comment.Text>{props.data.commentBody}</Comment.Text>
+            
         </Comment>
       </Comment.Group>
     </div>
