0073fba824f7c2e192b08b54730cc8ea27da5bf5
Vykintas.Petrikas <Vykintas.petrikas@gmail.com>
2020-11-13 12:54:47 +0200

    hopefully this fixes stuff for ross

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { API_URL } from "../config";
+import { Input } from "semantic-ui-react";
 import "./styles/EditProfile.css";
 
 function EditProfile(props) {
@@ -39,65 +40,82 @@ function EditProfile(props) {
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        props.onProfileEdit(e, ProfileImage);
-      }}>
-      <h3>Anything you would like to tell about yourself?</h3>
-      <input
-        name="aboutMe"
-        type="text"
-        onChange={(e) => {
-          handleInputChange(e, setAboutMe);
-        }}
-        value={AboutMe}></input>
-
-      <h3>What are your skills?</h3>
-      <input
-        name="mySkills"
-        type="text"
-        onChange={(e) => {
-          handleInputChange(e, setMySkills);
-        }}
-        value={MySkills}></input>
-      <div>
-        <div>
-          <img src={ProfileImage} style={{ width: "50px", height: "50px" }} />
-        </div>
-        <div>
-          {LinkButton ? (
+    <div className="main-cont">
+      <div className="form-cont">
+        <form
+          className="form"
+          onSubmit={(e) => {
+            props.onProfileEdit(e, ProfileImage);
+          }}>
+          <h3>Anything you would like to tell about yourself?</h3>
+          <div class="ui input">
+            <Input
+              name="aboutMe"
+              type="text"
+              onChange={(e) => {
+                handleInputChange(e, setAboutMe);
+              }}
+              value={AboutMe}></Input>
+          </div>
+          <h3>What are your skills?</h3>
+          <div class="ui input">
+            <Input
+              name="mySkills"
+              type="text"
+              onChange={(e) => {
+                handleInputChange(e, setMySkills);
+              }}
+              value={MySkills}></Input>
+          </div>
+          <div className='image-handler-cont'>
             <div>
-              <h3>Paste it here!</h3>
-              <input
-                onChange={(e) => {
-                  handleInputChange(e, setProfileImage);
-                }}
-                value={ProfileImage}
-                name="userImageLink"
-                type="text"></input>
-            </div>
-          ) : (
-            <div>
-              <h3>Upload your profile picture</h3>
-              <input
-                type="file"
-                className="form-control"
-                name="uploadedUserImage"
-                id="image"
+              <img
+                src={ProfileImage}
+                style={{ width: "50px", height: "50px" }}
               />
             </div>
-          )}
-          {LinkButton ? (
-            <button onClick={handleLinkButton}>
-              Or actually I want to upload
-            </button>
-          ) : (
-            <button onClick={handleLinkButton}>I have a link!</button>
-          )}
-        </div>
+            <div className="upload-link-handler-cont">
+              {LinkButton ? (
+                <div>
+                  <h3>Paste it here!</h3>
+                  <div class="ui input input-for-upload">
+                    <Input
+                      onChange={(e) => {
+                        handleInputChange(e, setProfileImage);
+                      }}
+                      value={ProfileImage}
+                      name="userImageLink"
+                      type="text"></Input>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <h3>Upload your profile picture</h3>
+                  <input
+                    type="file"
+                    className="form-control"
+                    name="uploadedUserImage"
+                    id="image"
+                  />
+                </div>
+              )}
+              {LinkButton ? (
+                <button className="textStyle" onClick={handleLinkButton}>
+                  Or actually I want to upload
+                </button>
+              ) : (
+                <button className="textStyle" onClick={handleLinkButton}>
+                  I have a link!
+                </button>
+              )}
+            </div>
+          </div>
+          <button className="textStyle" type="submit">
+            Edit your profile!
+          </button>
+        </form>
       </div>
-      <button type="submit">Edit your profile!</button>
-    </form>
+    </div>
   );
 }
 
