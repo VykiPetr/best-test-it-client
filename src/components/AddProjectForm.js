c4c42c7d194feed51c513d2d427527b8b3a39674
Vykintas.Petrikas <Vykintas.petrikas@gmail.com>
2020-11-13 13:20:45 +0200

    final push before presentning

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Select from "react-select";
+import { Input } from "semantic-ui-react";
 import "./styles/AddProjectForm.css";
 
 // import {appToolOptions} from '../toolsOptions.json'
@@ -45,67 +46,89 @@ function AddProjectForm(props) {
   };
 
   return (
-    <form onSubmit={props.onProjectAdd}>
-      <h3>What is the name of your project?</h3>
-      <input name="appName" type="text" placeholder="Awesome Project"></input>
+    <div className="main-cont">
+      <div className="form-cont">
+        <form className="form" onSubmit={props.onProjectAdd}>
+          <h3>What is the name of your project?</h3>
+          <div class="ui input">
+            <Input
+              name="appName"
+              type="text"
+              placeholder="Awesome Project"></Input>
+          </div>
+          <h3>Anything you would like to tell about it?</h3>
+          <div class="ui input">
+            <Input
+              name="appDescription"
+              type="text"
+              placeholder="It is Awesome!"></Input>
+          </div>
+          <h3>What are you using to create/develop this</h3>
+          <div class="ui input">
+            <Select
+              isMulti
+              name="appToolsData"
+              options={appToolOptions}
+              className="basic-multi-select"
+              classNamePrefix="select"
+            />
+          </div>
+          <h3>What is the link of the deployed project?</h3>
+          <div class="ui input">
+            <Input
+              name="deploymentLink"
+              type="text"
+              placeholder="awesome-project.herokuapp.com"></Input>
+          </div>
+          <h3>Where is the repository?</h3>
+          <div class="ui input">
+            <Input
+              name="repoLink"
+              type="text"
+              placeholder="github.com/john/doeProject"></Input>
+          </div>
+          <div className="upload-link-handler-cont">
+            {LinkButton ? (
+              <div>
+                <h3>Paste it here!</h3>
+                <div class="ui input input-for-upload">
+                  <Input name="appLogoLink" type="text"></Input>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <h3>Upload your logo</h3>
+                <input
+                  type="file"
+                  className="form-control"
+                  name="uploadedAppLogo"
+                  id="image"
+                />
+              </div>
+            )}
+          </div>
+          {LinkButton ? (
+            <button className="textStyle" onClick={handleLinkButton}>
+              I have a link!
+            </button>
+          ) : (
+            <button className="textStyle" onClick={handleLinkButton}>
+              Or actually I want to upload
+            </button>
+          )}
 
-      <h3>Anything you would like to tell about it?</h3>
-      <input
-        name="appDescription"
-        type="text"
-        placeholder="It is Awesome!"></input>
+          <h3>What version is it right now?</h3>
+          <Input
+            name="projectVersion"
+            type="text"
+            placeholder="0.0.0.0.0.0.1b"></Input>
 
-      <h3>What are you using to create/develop this</h3>
-      <Select
-        isMulti
-        name="appToolsData"
-        options={appToolOptions}
-        className="basic-multi-select"
-        classNamePrefix="select"
-      />
-
-      <h3>What is the link of the deployed project?</h3>
-      <input
-        name="deploymentLink"
-        type="text"
-        placeholder="awesome-project.herokuapp.com"></input>
-
-      <h3>Where is the repository?</h3>
-      <input
-        name="repoLink"
-        type="text"
-        placeholder="github.com/john/doeProject"></input>
-
-      {LinkButton ? (
-        <div>
-          <h3>Upload your logo</h3>
-          <input
-            type="file"
-            className="form-control"
-            name="uploadedAppLogo"
-            id="image"
-          />
-        </div>
-      ) : (
-        <div>
-          <h3>Paste it here!</h3>
-          <input name="appLogoLink" type="text"></input>
-        </div>
-      )}
-      {LinkButton ? (
-        <button onClick={handleLinkButton}>I have a link!</button>
-      ) : (
-        <button onClick={handleLinkButton}>Or actually I want to upload</button>
-      )}
-
-      <h3>What version is it right now?</h3>
-      <input
-        name="projectVersion"
-        type="text"
-        placeholder="0.0.0.0.0.0.1b"></input>
-
-      <button type="submit">Add this project!</button>
-    </form>
+          <button className="textStyle" type="submit">
+            Add this project!
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
